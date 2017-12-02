37a6d20520e20aa6456445929c39679eac0728e9
Mark Ayers <mark@philoserf.com>
2017-12-02 01:37:26 -0800

    Address lint in ipwildcards.go

--- ipwildcards.go
+++ ipwildcards.go
@@ -8,19 +8,10 @@ type ipwildcards struct {
 	ipw map[string]bool
 }
 
-func (ipw *ipwildcards) add(s string) {
-	ipw.ipw[s] = true
-}
-
 func (ipw *ipwildcards) addRange(ss []string) {
 	for _, s := range ss {
 		ipw.ipw[s] = true
 	}
-}
-
-func (ipw *ipwildcards) contains(s string) bool {
-	_, found := ipw.ipw[s]
-	return found
 }
 
 func (ipw *ipwildcards) containsAny(ss []string) bool {

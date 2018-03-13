265d03f5afcd7ad9e9e3e158df47b7fcabc9a7a4
BBerastegui <bberastegui@users.noreply.github.com>
2018-03-13 09:36:48 +0100

    Removed spaces from the UA files, error formatting

--- libgobuster/statehelpers.go
+++ libgobuster/statehelpers.go
@@ -175,11 +175,12 @@ func ValidateDirModeState(
 	if errorList.ErrorOrNil() == nil && s.RandomUserAgent {
 		b, err := ioutil.ReadFile(s.UserAgentsFile)
 		if err != nil {
-			errorList = multierror.Append(errorList, fmt.Errorf("[!] Error when reading the User-Agents file"))
+			errorList = multierror.Append(errorList, fmt.Errorf("[!] Error when reading the User-Agents file: %+v", err))
 		}
 		lines := strings.Split(string(b), "\n")
 		var uas []string
 		for _, line := range lines {
+			line = strings.TrimSpace(line)
 			if !strings.HasPrefix(line, "#") && len(line) > 0 {
 				uas = append(uas, line)
 			}

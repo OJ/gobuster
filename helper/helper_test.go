06ee44fb56d5069981b7e2128a698bd9c706786f
Christian Mehlmauer <firefart@gmail.com>
2019-04-17 08:57:49 +0200

    more benchmarks

--- helper/helper_test.go
+++ helper/helper_test.go
@@ -67,3 +67,50 @@ func TestParseStatusCodes(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkParseExtensions(b *testing.B) {
+	var tt = []struct {
+		testName           string
+		extensions         string
+		expectedExtensions libgobuster.StringSet
+		expectedError      string
+	}{
+		{"Valid extensions", "php,asp,txt", libgobuster.StringSet{Set: map[string]bool{"php": true, "asp": true, "txt": true}}, ""},
+		{"Spaces", "php, asp , txt", libgobuster.StringSet{Set: map[string]bool{"php": true, "asp": true, "txt": true}}, ""},
+		{"Double extensions", "php,asp,txt,php,asp,txt", libgobuster.StringSet{Set: map[string]bool{"php": true, "asp": true, "txt": true}}, ""},
+		{"Leading dot", ".php,asp,.txt", libgobuster.StringSet{Set: map[string]bool{"php": true, "asp": true, "txt": true}}, ""},
+		{"Empty string", "", libgobuster.NewStringSet(), "invalid extension string provided"},
+	}
+
+	for _, x := range tt {
+		b.Run(x.testName, func(b2 *testing.B) {
+			for y := 0; y < b2.N; y++ {
+				_, _ = ParseExtensions(x.extensions)
+			}
+		})
+	}
+}
+
+func BenchmarkParseStatusCodes(b *testing.B) {
+	var tt = []struct {
+		testName      string
+		stringCodes   string
+		expectedCodes libgobuster.IntSet
+		expectedError string
+	}{
+		{"Valid codes", "200,100,202", libgobuster.IntSet{Set: map[int]bool{100: true, 200: true, 202: true}}, ""},
+		{"Spaces", "200, 100 , 202", libgobuster.IntSet{Set: map[int]bool{100: true, 200: true, 202: true}}, ""},
+		{"Double codes", "200, 100, 202, 100", libgobuster.IntSet{Set: map[int]bool{100: true, 200: true, 202: true}}, ""},
+		{"Invalid code", "200,AAA", libgobuster.NewIntSet(), "invalid status code given: AAA"},
+		{"Invalid integer", "2000000000000000000000000000000", libgobuster.NewIntSet(), "invalid status code given: 2000000000000000000000000000000"},
+		{"Empty string", "", libgobuster.NewIntSet(), "invalid status code string provided"},
+	}
+
+	for _, x := range tt {
+		b.Run(x.testName, func(b2 *testing.B) {
+			for y := 0; y < b2.N; y++ {
+				_, _ = ParseStatusCodes(x.stringCodes)
+			}
+		})
+	}
+}

bdb34734d0d933611f49ae93b493623cfc637c27
yanir3 <tyanir@gmail.com>
2019-10-21 15:38:01 +0300

    Fix bug where the url option wasn't required in vhost busting

--- cli/cmd/http.go
+++ cli/cmd/http.go
@@ -25,7 +25,7 @@ func addCommonHTTPOptions(cmd *cobra.Command) error {
 	cmd.Flags().BoolP("insecuressl", "k", false, "Skip SSL certificate verification")
 	cmd.Flags().StringArrayP("headers", "H", []string{""}, "Specify HTTP headers, -H 'Header1: val1' -H 'Header2: val2'")
 
-	if err := cmdDir.MarkFlagRequired("url"); err != nil {
+	if err := cmd.MarkFlagRequired("url"); err != nil {
 		return fmt.Errorf("error on marking flag as required: %v", err)
 	}
 

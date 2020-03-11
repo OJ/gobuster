a4391d906bb88be87522b6eeb1a3574e2108389b
Christian Mehlmauer <firefart@gmail.com>
2020-03-11 21:40:29 +0100

    used wrong variable

--- gobusterdir/gobusterdir.go
+++ gobusterdir/gobusterdir.go
@@ -91,7 +91,7 @@ func (d *GobusterDir) PreRun() error {
 	guid := uuid.New()
 	url := fmt.Sprintf("%s%s", d.options.URL, guid)
 	if d.options.UseSlash {
-		url = fmt.Sprintf("%s/", d.options.URL)
+		url = fmt.Sprintf("%s/", url)
 	}
 
 	wildcardResp, _, _, err := d.http.Request(url, libgobuster.RequestOptions{})

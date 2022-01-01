e0056701b274046ec133cf3e6ac7b4ca4cef9668
00246506 <alberto.bregliano@telecomitalia.it>
2022-01-01 11:14:13 +0100

    addeded test for dir and simplified some functions

---
+++ gobusterdir/gobusterdir_test.go
@@ -0,0 +1,43 @@
+package gobusterdir
+
+import (
+	"testing"
+
+	"github.com/OJ/gobuster/v3/libgobuster"
+)
+
+const nothing = `[+] Url:        
+[+] Method:     
+[+] Threads:    0
+[+] Wordlist:   
+[+] Timeout:    0s`
+
+func TestGobusterDir_GetConfigString(t *testing.T) {
+	t.Parallel()
+	tests := []struct {
+		name    string
+		d       *GobusterDir
+		want    string
+		wantErr bool
+	}{
+		{"ok", &GobusterDir{
+			options:        &OptionsDir{},
+			globalopts:     &libgobuster.Options{},
+			http:           &libgobuster.HTTPClient{},
+			requestsPerRun: new(int),
+		}, nothing, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.d.GetConfigString()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GobusterDir.GetConfigString() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if got != tt.want {
+				t.Errorf("GobusterDir.GetConfigString() = %q, want %q", got, tt.want)
+			}
+		})
+	}
+}
